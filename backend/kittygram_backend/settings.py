98b5286f15dfacd0bdb0e3a5be5eaba220cf7d49
Andrey-0367 <shestakov0367@yandex.ru>
2023-05-23 18:02:59 +0300

    кошки легкий спринт

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'kittygram'),
         'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_NAME', ''),
+        'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
